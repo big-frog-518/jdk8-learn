87b54cf6814fa38979ec5feb5c36ed885bb68e58
jinxuZheng <1710198939@qq.com>
2021-10-24 20:52:29 +0800

    jdk8-learn-zjx day2

---
+++ demo/src/main/java/com/jdk8/demo/entity/User.java
@@ -0,0 +1,52 @@
+package com.jdk8.demo.entity;
+
+
+import lombok.Data;
+
+import java.util.Objects;
+
+/**
+ * @Author: ZhengJinxu
+ * @Date: 2021/10/24 20:22
+ */
+@Data
+public class User {
+
+    private String userName;
+
+    private String passWord;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        User user = (User) obj;
+        Boolean flag = Objects.equals(userName, user.userName) &&
+                Objects.equals(passWord, user.passWord);
+        return flag;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passWord, userName);
+    }
+
+    public User(String userName, String passWord) {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                '}';
+    }
+
+
+}
--- demo/src/test/java/com/jdk8/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/jdk8/demo/DemoApplicationTests.java
@@ -1,13 +1,108 @@
 package com.jdk8.demo;
 
+import com.jdk8.demo.entity.User;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
+    /**
+     * lambda练习
+     */
+    List<User> userList = new ArrayList<User>() {
+            {
+                add(new User("a", "b"));
+                add(new User("a1", "b1"));
+                add(new User("a222", "b2"));
+                add(new User("a", "bb"));
+                add(new User("a", "bbc"));
+                add(new User("a3", "b1"));
+                add(new User("a44", "b2"));
+            }
+        };
 
     @Test
-    void contextLoads() {
+    public void forEachTest() {
+        System.out.println("forEach遍历方式一");
+        userList.forEach(s -> System.out.print(s + " "));
+        System.out.println("forEach遍历方式二");
+        userList.forEach(System.out::println);
+    }
+    @Test
+    public void threadTest() {
+        // 继承Thread的形式
+        new Thread(() -> System.out.println("我是线程的run方法"))
+                .start();
+        // 实现runable的形式
+        Runnable r2 = () -> System.out.println("我也是线程的run方法");
+        r2.run();
+    }
+    @Test
+    public void sortTest() {
+        String[] players = {"1", "3", "2", "0"};
+        System.out.println("排序前");
+        List<String> list = Arrays.asList(players);
+        list.forEach(s -> System.out.print(s + " "));
+
+        Arrays.sort(players, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        System.out.println("\n排序后");
+        List<String> list1 = Arrays.asList(players);
+        list1.forEach(s -> System.out.print(s + " "));
     }
 
+    @Test
+    public void test() {
+        User maxUser=userList.stream().max((p1,p2)->p1.getUserName().length()-p2.getUserName().length()).get();
+        User minUser=userList.stream().min((p1,p2)->p1.getUserName().length()-p2.getUserName().length()).get();
+        System.out.println(maxUser+" \n"+minUser);
+    }
+
+    /**
+     * stream流
+     */
+    @Test
+    public void toSetTest() {
+        Set<String> setUser = userList.stream()
+                .map(User::getUserName)
+                .collect(Collectors.toSet());
+        setUser.forEach(s -> System.out.print(s + " "));
+    }
+
+    @Test
+    public void toTreeSetTest() {
+        TreeSet<String> treeSet = userList.stream()
+                .map(User::getUserName)
+                .collect(Collectors.toCollection(TreeSet::new));
+        treeSet.forEach(s -> System.out.print(s + " "));
+    }
+
+    @Test
+    public void filterTest() {
+        userList.stream()
+                .filter(p -> p.getUserName().equals("a"))
+                .limit(1)
+                .forEach(p -> System.out.println(p.getPassWord() + " "));
+
+        userList.stream()
+                .filter(p -> p.getUserName().equals("a"))
+                .forEach(p -> System.out.println(p.getPassWord() + " "));
+    }
+    @Test
+    public void test222() {
+        List<Integer> intNum= Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) ;
+        IntSummaryStatistics summaryStatistics=intNum.stream().mapToInt(x->x).summaryStatistics();
+        System.out.println(summaryStatistics.getCount()+" "+summaryStatistics.getAverage()+" "+summaryStatistics.getMax());
+    }
 }
+
+
+
+

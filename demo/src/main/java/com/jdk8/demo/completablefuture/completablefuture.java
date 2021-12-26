9cba670b708db9c977003aa880c1b613c853a1a7
jinxuZheng <1710198939@qq.com>
2021-12-26 21:33:10 +0800

    jdk8-learn-zjx day4

---
+++ demo/src/main/java/com/jdk8/demo/completablefuture/completablefuture.java
@@ -0,0 +1,43 @@
+package com.jdk8.demo.completablefuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: ZhengJinxu
+ * @Date: 2021/12/26 21:25
+ */
+
+public class completablefuture {
+
+    //无返回值
+    public static void runAsync() throws Exception {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("run end ...");
+        });
+
+        future.get();
+    }
+
+    //有返回值
+    public static void supplyAsync() throws Exception {
+        CompletableFuture<Long> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("run end ...");
+            return System.currentTimeMillis();
+        });
+
+        long time = future.get();
+        System.out.println("time = "+time);
+    }
+
+
+
+}
